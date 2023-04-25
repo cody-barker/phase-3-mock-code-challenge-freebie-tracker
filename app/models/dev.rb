6bd1d9e46c1991f62e8f759f17772622fc1df2b9
Cody <codybarker.or@gmail.com>
2023-04-25 15:24:39 -0700

    s

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,6 +3,12 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
 
     def received_one? item_name
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
+    end
+
+    def give_away dev, freebie
         
     end
 
